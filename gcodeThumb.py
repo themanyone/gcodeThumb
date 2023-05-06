405aae9a6f2dcb3ae43e3f3aecb57374b859774d
themanyone <nospam@thenerdshow.com>
2023-05-06 09:05:07 -0800

    2023 update

--- gcodeThumb.py
+++ gcodeThumb.py
@@ -2,7 +2,7 @@
 ##
 ## gcodeThumbnails
 ##
-## Copyright 2021 Henry Kroll <nospam@thenerdshow.com>
+## Copyright 2023 Henry Kroll <nospam@thenerdshow.com>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
